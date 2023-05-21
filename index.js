4bd0442c08382b89be90f65c2b697e479586a9d0
cozy1712 <kolawolekabiru89@gmail.com>
2023-05-21 07:27:33 +0100

    updated && add icon

--- index.js
+++ index.js
@@ -64,11 +64,3 @@ inputBtn.addEventListener("click", function() {
  
 
 
-
-// //////////////////checking trusy and falsy/////////////
-// console.log( Boolean("") )// false
-// console.log( Boolean("0") )// true
-// console.log( Boolean(100) )// true
-// console.log( Boolean(null) )// false
-// console.log( Boolean([0]) )//  true
-// console.log( Boolean(-0) )//   false
