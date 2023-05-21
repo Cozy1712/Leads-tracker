0b94b8cc640bdb292e78f9dc662a8cbbcfe423ac
cozy1712 <kolawolekabiru89@gmail.com>
2023-05-21 07:25:06 +0100

    updated && add icon

--- index.js
+++ index.js
@@ -2,29 +2,34 @@
 
 
 ///creating two variable
-let myLeads = []
-const inputEl = document.getElementById('input-el')
-const inputBtn = document.getElementById('input-btn')
-const ulEL = document.getElementById('ul-el')
+let myLeads = [];
+let oldLeads = [];
+const inputEl = document.getElementById('input-el');
+const inputBtn = document.getElementById('input-btn');
+const ulEL = document.getElementById('ul-el');
+const deleteBtn = document.getElementById('delete-btn');
 
-///////using eventlistner//////
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-   inputEl.value = " "  ////clear out input field
+///////// get leads from  local storage///////////
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log( leadsFromLocalStorage );
+
+if (leadsFromLocalStorage ){
+    myLeads = leadsFromLocalStorage
+    render(myLeads);
     
-    renderLeads()
-})
+}
 
-function renderLeads(){
+function render(leads){
     ////creating a listItem variable
     let listItems = ''
     
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i <leads.length; i++) {
         
         listItems += `
         <li>
-            <a target='_blank' href='${myLeads[i]}'>
-              ${myLeads[i]}
+            <a target='_blank' href='${leads[i]}'>
+              ${leads[i]}
             </a>
         </li>`
 
@@ -37,5 +42,33 @@ function renderLeads(){
 }
 
 
+//////// delete all bbutton ///////////////////
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+     myLeads = []
+    render(myLeads)
+})
+
+///////using eventlistner//////
+inputBtn.addEventListener("click", function() {
+   myLeads.push(inputEl.value)
+   inputEl.value = " "  ; ////clear out input field
 
 
+   localStorage.setItem("myLeads",JSON.stringify(myLeads))  ///////saving myleads array to local storage/////////
+    
+    render(myLeads)
+
+    console.log(localStorage.getItem("myLeads"))
+});
+ 
+
+
+
+// //////////////////checking trusy and falsy/////////////
+// console.log( Boolean("") )// false
+// console.log( Boolean("0") )// true
+// console.log( Boolean(100) )// true
+// console.log( Boolean(null) )// false
+// console.log( Boolean([0]) )//  true
+// console.log( Boolean(-0) )//   false
