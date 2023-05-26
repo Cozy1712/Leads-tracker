e4a07f31eb8639073620b977d30514f8c9acf2c0
cozy1712 <kolawolekabiru89@gmail.com>
2023-05-26 14:27:00 +0100

    updated

--- index.js
+++ index.js
@@ -3,22 +3,40 @@
 
 ///creating two variable
 let myLeads = [];
-let oldLeads = [];
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 const ulEL = document.getElementById('ul-el');
 const deleteBtn = document.getElementById('delete-btn');
-
+const tabBtn = document.getElementById('save-tab');
 ///////// get leads from  local storage///////////
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-console.log( leadsFromLocalStorage );
+//console.log( leadsFromLocalStorage );
+
 
 if (leadsFromLocalStorage ){
     myLeads = leadsFromLocalStorage
     render(myLeads);
     
 }
+// const tabs = [
+//     {url: "https://www.twitter.com/@cozy1712/"}
+// ]
+
+tabBtn.addEventListener("click", function (){
+    //Grab url from chrome browser///
+    // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    //   //console.log(tabs[0].url)
+    //     myLeads.push(tabs[0].url)
+    //     localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
+    //     render(myLeads) 
+    // });
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myleads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+    
+});
 
 function render(leads){
     ////creating a listItem variable
@@ -33,7 +51,7 @@ function render(leads){
             </a>
         </li>`
 
-        console.log(listItems)
+       // console.log(listItems)
     
     }
     //////////render the list item inside the ordered list using ulEl.innerHtml
@@ -53,14 +71,12 @@ deleteBtn.addEventListener("dblclick", function() {
 inputBtn.addEventListener("click", function() {
    myLeads.push(inputEl.value)
    inputEl.value = " "  ; ////clear out input field
-
-
    localStorage.setItem("myLeads",JSON.stringify(myLeads))  ///////saving myleads array to local storage/////////
-    
     render(myLeads)
 
-    console.log(localStorage.getItem("myLeads"))
+    //console.log(localStorage.getItem("myLeads"))
 });
  
+
 
 
